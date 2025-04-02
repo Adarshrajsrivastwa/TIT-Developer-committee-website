aaec904556efb30923c56077b34d3e9ec93203be
Aryaman Kushwaha <kushwaha.aryaman21@gmail.com>
2025-04-02 12:31:29 +0530

    Finished Mentor page

--- src/pages/Mentors.jsx
+++ src/pages/Mentors.jsx
@@ -3,10 +3,7 @@ import MentorsPage from '../components/Mentors/MentorProfiles'
 
 const Mentors = () => {
   return (
-    <div className='min-h-screen bg-gray-100 dark:bg-gray-900 '>
       <MentorsPage/>
-      </div>
-    
   )
 }
 
